d54429ffdf9a1d52c33be197b97d3b0ce53fbcf8
gilglecio <gilglecio_765@hotmail.com>
2016-11-04 13:01:19 -0300

    Threads

--- main.ino
+++ main.ino
@@ -1,29 +1,49 @@
+#include <Thread.h>
+#include <ThreadController.h>
 #include <Ultrasonic.h>
+
 #include <XMotor.h>
 #include <SmartCar.h>
 
 const int POT_PIN = A0;
-
 const int ULTRASON_ECHO_PIN = 12;
 const int ULTRASON_TRIGGER_PIN = 11;
-
 const int BUZZER_GND_PIN = A2;
 const int BUZZER_VCC_PIN = A5;
 
+int distance;
+
+ThreadController ctrl = ThreadController();
+Thread Actuators = Thread();
+Thread Sensors = Thread();
+
 XMotor M1(6, 4 ,7);
 XMotor M2(5, 2, 3);
-
 SmartCar Car(M1, M2);
-
 Ultrasonic Sensor(ULTRASON_TRIGGER_PIN, ULTRASON_ECHO_PIN);
+
+void actuatorCall() {
+
+    Car.setSpeed(150, 150);
+    Car.forward();
+
+    if (distance < 20){
+        bip();
+        Car.stop();
+        Car.backward();
+        Car.toTheLeft();
+    }
+}
+
+void sensorCall() {
+    distance = getDistance();
+}
 
 void setup () {
     
     pinMode(POT_PIN, INPUT);
-
     pinMode(ULTRASON_ECHO_PIN, INPUT);
     pinMode(ULTRASON_TRIGGER_PIN, OUTPUT);
-
     pinMode(BUZZER_VCC_PIN, OUTPUT);
     pinMode(BUZZER_GND_PIN, OUTPUT);
 
@@ -31,7 +51,15 @@ void setup () {
     digitalWrite(BUZZER_VCC_PIN, LOW);
 
     Car.stop();
-    Car.setSpeed(150, 150);
+    
+    Sensors.onRun(sensorCall);
+    Sensors.setInterval(100);
+
+    Actuators.onRun(actuatorCall);
+    Actuators.setInterval(100);
+
+    ctrl.add(&Actuators);
+    ctrl.add(&Sensors);
 }
 
 int bip() {
@@ -50,15 +78,5 @@ float getDistance() {
 }
 
 void loop () {
-
-    Car.forward();
-
-    if (getDistance() < 20){
-        
-        bip();
-
-        Car.stop();
-        Car.backward();
-        Car.toTheLeft();
-    }
+    ctrl.run();
 }
